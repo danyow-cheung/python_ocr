ddba17c57e3b791e1039dc2538becadd787a4077
MickeyCHAN <76671016+danyow-cheung@users.noreply.github.com>
2023-03-29 23:29:47 +0800

    Add files via upload

--- ANPR.py
+++ ANPR.py
@@ -37,9 +37,27 @@ print(location)
 mask = np.zeros(gray.shape,np.uint8)
 new_image = cv2.drawContours(mask,[location],0,255,-1)
 new_image = cv2.bitwise_and(img,img,mask=mask)
-plt.imshow(new_image)
+# plt.imshow(new_image)
+# plt.show()
+
+(x,y) = np.where(mask==255)
+(x1, y1) = (np.min(x), np.min(y))
+(x2, y2) = (np.max(x), np.max(y))
+cropped_image = gray[x1:x2+1, y1:y2+1]
+
+# plt.imshow(cropped_image)
+# plt.show()
+'''
+模型在线的？
+CUDA not available - defaulting to CPU. Note: This module is much faster with a GPU.
+Downloading detection model, please wait. This may take several minutes depending upon your network connection.
+'''
+reader = easyocr.Reader(['en'])
+result = reader.readtext(cropped_image)
+
+text = result[0][-2]
+font = cv2.FONT_HERSHEY_SIMPLEX
+res = cv2.putText(img, text=text, org=(approx[0][0][0], approx[1][0][1]+60), fontFace=font, fontScale=1, color=(0,255,0), thickness=2, lineType=cv2.LINE_AA)
+res = cv2.rectangle(img, tuple(approx[0][0]), tuple(approx[2][0]), (0,255,0),3)
+plt.imshow(cv2.cvtColor(res, cv2.COLOR_BGR2RGB))
 plt.show()
-
-
-(x,y) = np.where(mask=255)
-# (x1,y1) = np(np)
